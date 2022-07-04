cfdbab86dd24a8037c85ad1b42f4c37376da4d75
Hyeonae <aeae_1@naver.com>
2022-07-04 22:23:29 +0900

    [FEAT] #2 - 예외 처리

---
+++ server/src/main/kotlin/zzigmug/server/utils/exception/CustomExceptionHandler.kt
@@ -0,0 +1,14 @@
+package zzigmug.server.utils.exception
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+@RestControllerAdvice
+class CustomExceptionHandler: ResponseEntityExceptionHandler() {
+
+    @ExceptionHandler(value = [CustomException::class])
+    fun handleCustomException(e: CustomException): ResponseEntity<ErrorResponse> {
+        return ErrorResponse.toResponseEntity(e.errorCode)
+    }
+}
