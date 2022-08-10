d39b85dcfbaf6bab5ceb451cf700c93bb6d13f07
Hyeonae <aeae_1@naver.com>
2022-08-10 22:54:12 +0900

    [FEAT] OpenAPI 문서 설정, 접근 권한 설정

--- server/src/main/kotlin/zzigmug/server/config/OpenApiConfig.kt
+++ server/src/main/kotlin/zzigmug/server/config/OpenApiConfig.kt
@@ -1,4 +1,26 @@
 package zzigmug.server.config
 
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Contact
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.info.License
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
 class OpenApiConfig {
+
+    @Bean
+    fun openApi(): OpenAPI {
+        val info = Info()
+            .title("ZZIGMUG API")
+            .version("1.0")
+            .description("숙명여자대학교 19학번 졸업작품 찍먹")
+            .termsOfService("http://swagger.io/terms/")
+            .contact(Contact().name("Hyeonae Jang").url("https://github.com/raae7742").email("raae7742@gmail.com"))
+            .license(License().name("Apache License Version 2.0").url("http://www.apache.org/licenses/LICENSE-2.0"))
+
+        return OpenAPI().components(Components()).info(info)
+    }
 }
--- server/src/main/kotlin/zzigmug/server/config/SecurityConfig.kt
+++ server/src/main/kotlin/zzigmug/server/config/SecurityConfig.kt
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import zzigmug.server.config.jwt.*
 
@@ -19,6 +21,9 @@ class SecurityConfig(
     private val jwtAccessDeniedHandler: JwtAccessDeniedHandler,
     private val jwtTokenProvider: JwtTokenProvider,
 ): WebSecurityConfigurerAdapter() {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder()
 
     @Bean
     @Throws(Exception::class)
@@ -48,8 +53,32 @@ class SecurityConfig(
 
             .and()
             .authorizeRequests()
-            .antMatchers("/h2-console/**", "/**").permitAll()
-            .anyRequest().permitAll()
+            .antMatchers(
+                "/h2-console/**",
+                "/swagger-ui.html",
+                "/swagger-ui/**",
+                "/swagger-resources",
+                "/swagger-resources/**",
+                "configuration/ui",
+                "configuration/security",
+                "/favicon.ico",
+                "/webjars/**",
+                "/v2/api-docs",
+                "/v3/api-docs/**",
+                "/login/kakao/callback",
+                "/join",
+                "/check/nickname",
+            ).permitAll()
+
+            .and()
+            .authorizeRequests()
+            .antMatchers("/admin/**")
+            .access("hasAnyRole('ROLE_ADMIN')")
+
+            .and()
+            .authorizeRequests()
+            .anyRequest()
+            .authenticated()
 
             .and()
             .addFilterBefore(JwtFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter::class.java)
